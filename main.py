575170f456512c6b3c8acda5b4eec1dd39901b02
Gabriel Sroka <gabrielsroka@gmail.com>
2022-04-07 09:01:34 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,40 +3,27 @@
 import requests
 import os
 
-OKTA_URL = os.environ['OKTA_URL']
+OKTA_URL = 'https://' + os.environ['OKTA_URL']
 OKTA_API_KEY = os.environ['OKTA_API_KEY']
 
 headers = {
     'Accept': 'application/json',
-    'Content-Type': 'application/json',
     'Authorization': f"SSWS {OKTA_API_KEY}",
 }
+session = requests.Session()
+session.headers.update(headers)
 
-def pagination_value(response):
-    after_link = response.headers['link'].split(',', 1)[1]
-    after_curser = after_link.split('after=')[1].split('&')[0]
-    return after_curser
-
-response = requests.get(f'https://{OKTA_URL}/api/v1/users', headers=headers)
-user_list = response.json()
-next_page = pagination_value(response)
-
-# Pagination
-while len(user_list) >= 200:
-    next_response = requests.get(f'https://{OKTA_URL}/api/v1/users?after={next_page}', headers=headers)
-    next_list = next_response.json()
-    user_list.extend(next_list)
-    if len(next_list) < 200:
-        break
-    next_page = pagination_value(next_response)
-    
-print("Total Users: " + str(len(user_list)))
-    
-for i in user_list:
-    user_id=i['id']
-    user_email=i['profile']['email']
-    print(f"User emai:{user_email} ID: {user_id}")
-    expire_pw_response = requests.post(f'https://{OKTA_URL}/api/v1/users/{user_id}/lifecycle/expire_password', headers=headers)
-    print(f"pw expired respnse: {expire_pw_response}")
-    expire_session_response = requests.delete(f'https://{OKTA_URL}/api/v1/users/{user_id}/sessions', headers=headers)
-    print(f"session expire response: {expire_session_response}")
+url = f'{OKTA_URL}/api/v1/users'
+while url:
+    response = session.get(url)
+    users = response.json()
+    for user in users:
+        user_id = user['id']
+        user_email = user['profile']['email']
+        print(f"User email: {user_email} ID: {user_id}")
+        expire_pw_response = session.post(f'{OKTA_URL}/api/v1/users/{user_id}/lifecycle/expire_password')
+        print(f"pw expired response: {expire_pw_response}")
+        expire_session_response = session.delete(f'{OKTA_URL}/api/v1/users/{user_id}/sessions')
+        print(f"session expire response: {expire_session_response}")
+    next = response.links.get('next')
+    url = next['url'] if next else None
